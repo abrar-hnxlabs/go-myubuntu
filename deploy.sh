58efd9dc2aff01d9f6adb70d1372c21ca8608bb3
abrar-hnxlabs <abrar@hnxlabs.com>
2019-08-21 13:58:55 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
+echo "writing private file"
 echo $SSH_PRIV_KEY > ~/.ssh/priv_key.b64
 base64 -d ~/.ssh/priv_key.b64 > ~/.ssh/travis.id_rsa
 chmod 600 ~/.ssh/travis.id_rsa
-ls -al
+echo "starting transfer"
 rsync -Pav -e "ssh -i ~/.ssh/travis.id_rsa -p 4430" go-myubuntu abrar@plex.hnxlabs.com:~/bin/
+echo "tranfer done"
