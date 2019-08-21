a9493dffb0e9bdc022d5997efe288ce83b76ed02
abrar-hnxlabs <abrar@hnxlabs.com>
2019-08-21 12:54:57 -0700

    add new priv keys

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 echo $SSH_PRIV_KEY > priv_key.b64
-cat priv_key.b64
 base64 -d priv_key.b64 > priv.key
-cat priv.key
-scp -i priv.key -P 4430 go-myubuntu abrar@plex.hnxlabs.com:~/bin/
+scp -y -i priv.key -P 4430 go-myubuntu abrar@plex.hnxlabs.com:~/bin/
