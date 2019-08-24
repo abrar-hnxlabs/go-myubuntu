318c8a994d26febd7855c8cc19caac91207519fb
abrar-hnxlabs <abrar@hnxlabs.com>
2019-08-24 01:23:09 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 echo $SSH_PRIV_KEY > ~/.ssh/priv_key.b64
 base64 -d ~/.ssh/priv_key.b64 > ~/.ssh/travis.id_rsa
 chmod 600 ~/.ssh/travis.id_rsa
-rsync -Pav -e "ssh -i ~/.ssh/travis.id_rsa -p 4430" go-myubuntu abrar@plex.hnxlabs.com:~/bin/hnxapp
+rsync -Pav -e "ssh -i ~/.ssh/travis.id_rsa -p 4430" go-hnx abrar@plex.hnxlabs.com:~/bin/hnxapp
