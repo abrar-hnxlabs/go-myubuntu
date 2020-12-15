971ecfe494002cc3a2734b70cc76b88bdc558469
Abrar <abrar.ahmed@bcgdv.com>
2020-12-14 19:31:49 -0800

    add plex download url

---
+++ commands/plex.go
@@ -0,0 +1,33 @@
+package commands
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+
+	"github.com/levigross/grequests"
+)
+
+var apiResp map[string]interface{}
+
+// Plexurl comment explaingin
+func Plexurl() {
+	downloadURL := "https://plex.tv/api/downloads/5.json"
+	resp, err := grequests.Get(downloadURL, nil)
+	if err != nil {
+		log.Println("Unable to fetch plex api.")
+		return
+	}
+
+	//releases := resp.JSON()["computer"]["Linus"]["releases"]
+	resp.JSON(&apiResp)
+	releases := apiResp["computer"].(map[string]interface{})["Linux"].(map[string]interface{})["releases"].([]interface{})
+	for _, i := range releases {
+		furl := i.(map[string]interface{})["url"].(string)
+		matched, _ := regexp.MatchString(`^https:.*_arm64.deb$`, furl)
+		if matched == true {
+			fmt.Println(furl)
+			return
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,85 +1,91 @@
 package main
+
 import (
-  "github.com/teris-io/cli"
-  "os"
-  "log"
-  "strings"
-  "github.com/abrar-hnxlabs/go-hnx/commands"
+	"os"
+	"strings"
+
+	"github.com/abrar-hnxlabs/go-hnx/commands"
+	"github.com/teris-io/cli"
 )
 
 func main() {
-  log.Println("Starting App")
-  dns := cli.NewCommand("dns", "update google domains").
-    WithAction(func(args []string, options map[string]string ) int {
-      result, _ := commands.UpdateDns()
-      return result
-    })
+	dns := cli.NewCommand("dns", "update google domains").
+		WithAction(func(args []string, options map[string]string) int {
+			result, _ := commands.UpdateDns()
+			return result
+		})
 
-  encrypt := cli.NewCommand("encrypt", "Encrypt a given file").
-    WithOption(cli.NewOption("in", "input file name").WithType(cli.TypeString)).
-    WithOption(cli.NewOption("p", "password to encrypt file.").WithType(cli.TypeString)).
-    WithAction(func(args []string, options map[string]string ) int{
-      filename := options["in"]
-      password := options["p"]
-      password32chars := password + strings.Repeat("X",32-len(password))
-      commands.EncryptFile(filename, password32chars)
-      return 0
-    })
+	encrypt := cli.NewCommand("encrypt", "Encrypt a given file").
+		WithOption(cli.NewOption("in", "input file name").WithType(cli.TypeString)).
+		WithOption(cli.NewOption("p", "password to encrypt file.").WithType(cli.TypeString)).
+		WithAction(func(args []string, options map[string]string) int {
+			filename := options["in"]
+			password := options["p"]
+			password32chars := password + strings.Repeat("X", 32-len(password))
+			commands.EncryptFile(filename, password32chars)
+			return 0
+		})
 
-  decrypt := cli.NewCommand("decrypt", "Decrypt a given file").
-  WithOption(cli.NewOption("in", "input file name").WithType(cli.TypeString)).
-  WithOption(cli.NewOption("p", "passwordto decrypt file").WithType(cli.TypeString)).
-  WithAction(func(args []string, options map[string]string ) int{
-    filename := options["in"]
-    password := options["p"]
-    password32chars := password + strings.Repeat("X",32-len(password))
-    commands.Decryptfile(filename,password32chars)
-    return 0
-  })
+	decrypt := cli.NewCommand("decrypt", "Decrypt a given file").
+		WithOption(cli.NewOption("in", "input file name").WithType(cli.TypeString)).
+		WithOption(cli.NewOption("p", "passwordto decrypt file").WithType(cli.TypeString)).
+		WithAction(func(args []string, options map[string]string) int {
+			filename := options["in"]
+			password := options["p"]
+			password32chars := password + strings.Repeat("X", 32-len(password))
+			commands.Decryptfile(filename, password32chars)
+			return 0
+		})
 
-  docker := cli.NewCommand("docker", "update plex restart docker").
-    WithAction(func(args []string, options map[string]string ) int {
-      commands.UpdateAndRestartDocker()
-      return 0
-    })
+	docker := cli.NewCommand("docker", "update plex restart docker").
+		WithAction(func(args []string, options map[string]string) int {
+			commands.UpdateAndRestartDocker()
+			return 0
+		})
 
-  dc := cli.NewCommand("dc", "proxy docker-compose commands").
-  WithOption(cli.NewOption("c", "docker command to run").WithType(cli.TypeString)).
-  WithAction(func(args []string, options map[string]string ) int {
-    commands.RunDockerInstance(options["c"])
-    return 0
-  })
+	dc := cli.NewCommand("dc", "proxy docker-compose commands").
+		WithOption(cli.NewOption("c", "docker command to run").WithType(cli.TypeString)).
+		WithAction(func(args []string, options map[string]string) int {
+			commands.RunDockerInstance(options["c"])
+			return 0
+		})
 
+	slugs := cli.NewCommand("sluggify", "sluggify the filenames.").
+		WithOption(cli.NewOption("r", "root folder to start scan from.").WithType(cli.TypeString)).
+		WithAction(func(args []string, options map[string]string) int {
+			commands.SlugifyFiles(options["r"])
+			return 0
+		})
 
-  slugs := cli.NewCommand("sluggify", "sluggify the filenames.").
-  WithOption(cli.NewOption("r", "root folder to start scan from.").WithType(cli.TypeString)).
-  WithAction(func(args []string, options map[string]string ) int {
-    commands.SlugifyFiles(options["r"])
-    return 0
-  })
+	duper := cli.NewCommand("duper", "find files for duper.").
+		WithOption(cli.NewOption("r", "root folder to scan dupes").WithType(cli.TypeString)).
+		WithAction(func(args []string, options map[string]string) int {
+			commands.Duper(options["r"])
+			return 0
+		})
 
-  duper := cli.NewCommand("duper", "find files for duper.").
-  WithOption(cli.NewOption("r", "root folder to scan dupes").WithType(cli.TypeString)).
-  WithAction(func(args []string, options map[string]string) int {
-    commands.Duper(options["r"])
-    return 0
-  })
+	getip := cli.NewCommand("ip", "Get outside world ip.").
+		WithAction(func(args []string, options map[string]string) int {
+			commands.GetIp()
+			return 0
+		})
 
-  getip := cli.NewCommand("ip", "Get outside world ip.").
-  WithAction(func(args []string, options map[string]string) int {
-    commands.GetIp()
-    return 0
-  })
+	getplexurl := cli.NewCommand("plex", "Get plex download url.").
+		WithAction(func(args []string, options map[string]string) int {
+			commands.Plexurl()
+			return 0
+		})
 
-  app := cli.New("Version: 1.1.0").
-    WithCommand(dns).
-    WithCommand(encrypt).
-    WithCommand(decrypt).
-    WithCommand(docker).
-    WithCommand(dc).
-    WithCommand(slugs).
-    WithCommand(duper).
-    WithCommand(getip)
+	app := cli.New("Version: 1.1.0").
+		WithCommand(dns).
+		WithCommand(encrypt).
+		WithCommand(decrypt).
+		WithCommand(docker).
+		WithCommand(dc).
+		WithCommand(slugs).
+		WithCommand(duper).
+		WithCommand(getip).
+		WithCommand(getplexurl)
 
-  os.Exit(app.Run(os.Args, os.Stdout))
+	os.Exit(app.Run(os.Args, os.Stdout))
 }
